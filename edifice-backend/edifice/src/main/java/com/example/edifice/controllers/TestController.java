a1dba821177f5e48227ac6fd208533cdea1652dc
Sahan Dissanayaka <tsahandisaai@gmail.com>
2021-06-03 15:36:12 +0530

    Fixes: Login Authorization

--- edifice-backend/edifice/src/main/java/com/example/edifice/controllers/TestController.java
+++ edifice-backend/edifice/src/main/java/com/example/edifice/controllers/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
+
 public class TestController {
 	@GetMapping("/all")
 	public String allAccess() {
@@ -16,19 +17,19 @@ public class TestController {
 	}
 	
 	@GetMapping("/user")
-	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	@PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_MODERATOR') or hasRole('ROLE_ADMIN')")
 	public String userAccess() {
 		return "User Content.";
 	}
 
 	@GetMapping("/mod")
-	@PreAuthorize("hasRole('MODERATOR')")
+	@PreAuthorize("hasRole('ROLE_MODERATOR')")
 	public String moderatorAccess() {
 		return "Moderator Board.";
 	}
 
 	@GetMapping("/admin")
-	@PreAuthorize("hasRole('ADMIN')")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public String adminAccess() {
 		return "Admin Board.";
 	}
