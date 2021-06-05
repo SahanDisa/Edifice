282c6bde119299df5756aeac410e174f695300f9
Sahan Dissanayaka <tsahandisaai@gmail.com>
2021-06-06 00:38:20 +0530

    Add: Backend Project component CRUD JPA

---
+++ edifice-backend/edifice/src/main/java/com/example/edifice/models/Project.java
@@ -0,0 +1,63 @@
+package com.example.edifice.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "project")
+
+public class Project {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name ="title")
+	private String title;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "published")
+	private boolean published;
+	
+	public Project() {
+		
+	}
+	public Project(String title,String description, boolean published) {
+		this.title = title;
+		this.description = description;
+		this.published = published;
+	}
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isPublished() {
+		return published;
+	}
+
+	public void setPublished(boolean isPublished) {
+		this.published = isPublished;
+	}
+
+	@Override
+	public String toString() {
+		return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+	}
+}
