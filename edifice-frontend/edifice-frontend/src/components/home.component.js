29a923f1f56bbc445acfcf8d7e29fa1f35fa741b
ov1n <thenukaovin@gmail.com>
2021-06-15 22:26:05 +0530

    Fixed component issues

--- edifice-frontend/edifice-frontend/src/components/home.component.js
+++ edifice-frontend/edifice-frontend/src/components/home.component.js
@@ -36,43 +36,12 @@ export default class Home extends Component {
           <h3>Home</h3>
           <h5>Projects that you involved in</h5>
           {/* Display involved Project of a particular user */}
-          <div class="card">
-            <h5 class="card-header">Project XX1</h5>
-            <div class="card-body">
-              <h5 class="card-title">Port City: Apartment Section 01</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="/addproject" className="btn btn-primary"> Go to the project</a>
-            </div>
-          </div>
+
           <div class="card">
             <h5 class="card-header">Project XX2</h5>
             <div class="card-body">
               <h5 class="card-title">Port City: Apartment Section 01</h5>
               <p class="card-text">Random description.</p>
-              {/* Table of people involved in project */}
-              <table class="table table-bordered">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Role</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Office</th>
-                    <th scope="col">Mobile</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>{/* Role here */}</td>
-                    <td>{/* Name here */}</td>
-                    <td>{/* Email here */}</td>
-                    <td>{/* Office here */}</td>
-                    <td>{/* Mobile here */}</td>
-                  </tr>
-                </tbody>
-              </table>
-
               <a href="/projext" className="btn btn-primary"> Go to the project</a>
             </div>
           </div>
