618eaf7bf8ecb1aac626329543fcf17339fd3085
malithya_fernando <56259098+MalithyaFernando@users.noreply.github.com>
2021-06-29 14:26:18 +0530

    Update: Manage and Configuration tools in Task

--- edifice-frontend/edifice-frontend/src/components/project_management/meetings/update.component.js
+++ edifice-frontend/edifice-frontend/src/components/project_management/meetings/update.component.js
@@ -1,4 +1,56 @@
 import React, { Component } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const data = [
+  {edit: <a href="/managesmeetings/update" className="btn btn-outline-success">Edit</a>}
+];
+const columns = [
+  {
+    dataField: 'edit',
+    text: '',
+    headerStyle: (column, colIndex) => {
+        return { width: '7%', textAlign: 'center' };}
+  }, {
+    dataField: 'view',
+    text: '',
+    headerStyle: (column, colIndex) => {
+    return { width: '7%', textAlign: 'center' };}
+  }, {
+    dataField: 'overview',
+    text: '',
+    headerStyle: (column, colIndex) => {
+    return { width: '50%', textAlign: 'center' };}
+  }, {
+    dataField: 'date',
+    text: '',
+    headerStyle: (column, colIndex) => {
+        return { width: '10%', textAlign: 'center' };}
+  }, {
+    dataField: 'time',
+    text: '',
+    headerStyle: (column, colIndex) => {
+    return { width: '7%', textAlign: 'center' };}
+  }, {
+    dataField: 'location',
+    text: '',
+    headerStyle: (column, colIndex) => {
+    return { width: '7%', textAlign: 'center' };}
+  }, {
+    dataField: 'status',
+    text: '',
+    headerStyle: (column, colIndex) => {
+        return { width: '7%', textAlign: 'center' };}
+  }, {
+    dataField: 'items',
+    text: '',
+    headerStyle: (column, colIndex) => {
+    return { width: '7%', textAlign: 'center' };}
+  }, {
+    dataField: 'extra',
+    text: '',
+    headerStyle: (column, colIndex) => {
+        return { width: '7%', textAlign: 'center' };}
+  }];
 
 class UpdateMeetings extends Component {
 
@@ -6,17 +58,17 @@ class UpdateMeetings extends Component {
     return (
       <div className="">
         <h2>Update Meeting</h2><hr/>
-        <div className="mt-3">
+        <div className="mt-3 mb-3">
         <h5>Meeting Name</h5>
           <form>
             <div className="form-row">
                 <div className="form-group col-md-12 text-right">
                     <div className="form-check">
-                        <input type="checkbox" className="form-check-input" id="draftCheck" />
+                        <input type="checkbox" className="form-check-input" id="draftCheck" required/>
                         <label htmlFor="draftCheck" className="form-check-label">Draft Meeting</label>
                     </div>
                     <div className="form-check">
-                        <input type="checkbox" className="form-check-input" id="draftCheck" />
+                        <input type="checkbox" className="form-check-input" id="draftCheck" required/>
                         <label htmlFor="draftCheck" className="form-check-label">Private Meeting</label>
                     </div>
                 </div>                
@@ -52,7 +104,7 @@ class UpdateMeetings extends Component {
             <div className="form-row">
                 <div className="form-group col-md-6">
                     <label htmlFor="">Overview</label>
-                    <textarea className="form-control" name="" id=""></textarea>
+                    <textarea className="form-control" name="" id="" required></textarea>
                 </div>
             </div>
             <div className="form-row mb-3">
@@ -67,6 +119,49 @@ class UpdateMeetings extends Component {
             <a href="/managetasks" type="submit" className="btn btn-success mr-2">Save</a>
             <a href="/managetasks" type="submit" className="btn btn-danger">Cancel</a>
           </form>
+        </div>
+        <div class="accordion" id="accordionExample">
+          <h5>Agenda</h5>
+          <div class="card">
+            <div class="card-header" id="headingOne">
+              <h2 class="mb-0">
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Agenda item #1</button>
+              </h2>
+            </div>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div class="card-body">
+                <div className="">
+                  <BootstrapTable 
+                    hover
+                    keyField='location'
+                    data={ data }
+                    columns={ columns } 
+                    cellEdit={ false }
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header" id="headingTwo">
+              <h2 class="mb-0">
+                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Agenda item #2</button>
+              </h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+              <div class="card-body">
+                <div className="">
+                  <BootstrapTable 
+                    hover
+                    keyField='location'
+                    data={ data }
+                    columns={ columns } 
+                    cellEdit={ false }
+                  />
+                </div>
+              </div>
+            </div>
+          </div>  
         </div>
       </div>
     );
