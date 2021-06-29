08eb850917b8130d78e6abac8dade7e123b2a4be
vinuri-s <piyathilakev@gmail.com>
2021-06-29 13:58:41 +0530

    Add: Budget Component basic UIs

---
+++ edifice-frontend/edifice-frontend/src/components/financial_component/prime-contracts/prime-contracts.component.js
@@ -0,0 +1,18 @@
+
+
+const PrimeContracts = () => {
+
+    return (
+        <div>
+        <h2>New Prime Contract</h2><hr/>
+        <div className="mb-3">
+          <h5>New Tasks</h5>
+          </div>
+        </div>
+
+    );
+
+
+}
+
+export default PrimeContracts;
