c909bc53281c684de28cc8129ef6131c06dace24
vinuri-s <piyathilakev@gmail.com>
2021-07-11 18:09:20 +0530

    Add: Minor UI Changes

--- edifice-frontend/edifice-frontend/src/components/financial_management/financial-manage-home.component.js
+++ edifice-frontend/edifice-frontend/src/components/financial_management/financial-manage-home.component.js
@@ -3,16 +3,13 @@ import { Switch, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import UserService from "./../../services/user.service";
 
-import portfolioIcon from "././../../assets/portfolio.png";
-import rfiIcon from "././../../assets/rfi.png";
-import dailylogIcon from "././../../assets/dailylog.png";
-import meetingIcon from "././../../assets/meeting.png";
-import biddingIcon from "././../../assets/bidding.png";
-import actionplanIcon from "././../../assets/actionplan.png";
-import drawingsIcon from "././../../assets/drawings.png";
-import photosIcon from "././../../assets/photos.png";
-import punchlistIcon from "././../../assets/punchlist.png";
-import documentIcon from "././../../assets/documents.png";
+
+import budgetIcon from "././../../assets/FM/budget.png";
+import primecontractsIcon from "././../../assets/FM/primecontract.png";
+import costIcon from "././../../assets/FM/cost.png";
+import invoiceIcon from "././../../assets/FM/invoice.png";
+import commitmentsIcon from "././../../assets/FM/commitments.png";
+
 
 export default class BoardUser extends Component {
   constructor(props) {
@@ -55,44 +52,42 @@ export default class BoardUser extends Component {
         </header>
 
         <div className="row">
-            <div className="col-lg-4 col-sm-6 mb-grid-gutter pb-2">
-            <div className="card card-hover shadow-sm">
-            <div className="d-block nav-heading text-center mt-2 mb-2 card card-hover shadow-sm">
+        <div className="col-lg-4 col-sm-6 mb-grid-gutter pb-2">
+              <div className="d-block nav-heading text-center mt-2 mb-2 card card-hover shadow-sm">
               <Link
                 to={"/budget/" + id}
               
               >
-                <img src={drawingsIcon} alt="" width="50"/>
+                <img src={budgetIcon} alt="" width="50"/><br />
                 <h3 className="h5 nav-heading-title mb-0">Budget</h3>
-                <span className="fs-sm fw-normal text-muted">Manage your project drawings in one place</span>
+                <span className="fs-sm fw-normal text-muted">Set up and manage a comprehensive budget throughout the lifecycle of a project.</span>
               </Link>
               </div>
-            </div>
             </div>
             <div className="col-lg-4 col-sm-6 mb-grid-gutter pb-2">
               <div className="card card-hover shadow-sm">
               <a className="d-block nav-heading text-center mt-2 mb-2" href="/prime-contracts">
-                <img src={rfiIcon} alt="" width="50"/>
+                <img src={primecontractsIcon} alt="" width="50"/>
                 <h3 className="h5 nav-heading-title mb-0">Prime Contracts</h3>
-                <span className="fs-sm fw-normal text-muted">Help to run the project smoothly and on schedule</span>
+                <span className="fs-sm fw-normal text-muted">Easily create and manage contracts with the clients.</span>
               </a>
               </div>
             </div>
             <div className="col-lg-4 col-sm-6 mb-grid-gutter pb-2">
               <div className="card card-hover shadow-sm">
               <a className="d-block nav-heading text-center mt-2 mb-2" href="/direct-costs">
-                <img src={dailylogIcon} alt="" width="50"/>
+                <img src={costIcon} alt="" width="50"/>
                 <h3 className="h5 nav-heading-title mb-0">Direct Costs</h3>
-                <span className="fs-sm fw-normal text-muted">Keep track of every detail at job site each and everyday</span>
+                <span className="fs-sm fw-normal text-muted">Track all direct costs that are not associated with commitments.</span>
               </a>
               </div>
             </div>
             <div className="col-lg-4 col-sm-6 mb-grid-gutter pb-2">
               <div className="card card-hover shadow-sm">
               <a className="d-block nav-heading text-center mt-2 mb-2" data-toggle="modal" data-target="#meetingModal" href="/commitments">
-                <img src={meetingIcon} alt="" width="50"/>
+              <img src={commitmentsIcon} alt="" width="50"/>
                 <h3 className="h5 nav-heading-title mb-0">Commitments</h3>                
-                <span className="fs-sm fw-normal text-muted">Manage all aspects of your project meetings from agenda distribution</span>
+                <span className="fs-sm fw-normal text-muted">Allows seeing the status and current value of all contracts and purchase orders.</span>
               </a>
               </div>
             </div>
@@ -102,20 +97,11 @@ export default class BoardUser extends Component {
                 to={"/invoicing/" + id}
               
               >
-                <img src={biddingIcon} alt="" width="50"/>
+                <img src={invoiceIcon} alt="" width="50"/>
                 <h3 className="h5 nav-heading-title mb-0">Invoicing</h3>
-                <span className="fs-sm fw-normal text-muted">Manage all the bid packages and bidding proceses</span>
+                <span className="fs-sm fw-normal text-muted">Streamline the invoice collection, review, and approval process on all of the projects</span>
                 
               </Link>
-              </div>
-            </div>
-            <div className="col-lg-4 col-sm-6 mb-grid-gutter pb-2">
-              <div className="card card-hover shadow-sm">
-              <a className="d-block nav-heading text-center mt-2 mb-2" href="/actionplan">
-                <img src={actionplanIcon} alt="" width="50"/>
-                <h3 className="h5 nav-heading-title mb-0"></h3>
-                <span className="fs-sm fw-normal text-muted">Clearly defined, centralized, and organized the project-specific requirements</span>
-              </a>
               </div>
             </div>
           </div>
