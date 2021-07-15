620bf6e340077dfeb534ebf19fafb8ab69c8ffc7
vinuri-s <piyathilakev@gmail.com>
2021-07-15 10:01:28 +0530

    Update: Invoice UI Modifications

--- edifice-frontend/edifice-frontend/src/components/financial_management/invoicing/invoicing.component.js
+++ edifice-frontend/edifice-frontend/src/components/financial_management/invoicing/invoicing.component.js
@@ -1,75 +1,65 @@
+import { Link } from "react-router-dom";
 import BootstrapTable from 'react-bootstrap-table-next';
 import SettingsIcon from '@material-ui/icons/Settings';
 
 const data = [
-  {edit:<a href="/direct-costs/edit" className="btn btn-success">Edit</a>, view:<a href="/direct-costs/view" className="btn btn-primary">View</a>, date: "05/04/21", vendor:"Vertex Properties", type:"Expense", invoice:"C001",status:"Approved",ammount:"$1,250.00",receivedDate:"05/04/21",paidDate:"08/04/21",blank:"",delete:<a href="/direct-costs/edit" className="btn btn-danger">Delete</a>},
-  {edit:<a href="/direct-costs/edit" className="btn btn-success">Edit</a>, view:<a href="/direct-costs/view" className="btn btn-primary">View</a>, date: "05/05/21", vendor:"Tesen Cement", type:"Expense", invoice:"C012",status:"Draft",ammount:"$1,250.00",receivedDate:"21/05/21",paidDate:"25/05/21",blank:"",delete:<a href="/direct-costs/edit" className="btn btn-danger">Delete</a>},
-  {edit:<a href="/direct-costs/edit" className="btn btn-success">Edit</a>, view:<a href="/direct-costs/view" className="btn btn-primary">View</a>, date: "18/05/21", vendor:"Fusion", type:"Expense", invoice:"C020",status:"Approved",ammount:"$1,250.00",receivedDate:"18/05/21",paidDate:"20/05/21",blank:"",delete:<a href="/direct-costs/edit" className="btn btn-danger">Delete</a>},
-  {edit:<a href="/direct-costs/edit" className="btn btn-success">Edit</a>, view:<a href="/direct-costs/view" className="btn btn-primary">View</a>, date: "20/06/21", vendor:"Xavie", type:"Expense", invoice:"C002",status:"Approved",ammount:"$1,250.00",receivedDate:"20/06/21",paidDate:"24/06/21",blank:"",delete:<a href="/direct-costs/edit" className="btn btn-danger">Delete</a>}
+  {checkbox: <input type="checkbox" />, commitment:<a href="#">SC-01-001</a>,  contractCompany:"Vertex Properties", billingPeriod: "15/07/2021-15/08/2021",invoiceStatus:<a href="#">Draft</a>, invoiceDates:"15/07/2021-15/08/2021",contractValue:"20,000",complete:"0.00%",currentPaymentDue:"0.00",paymentStatus:"Unpaid"},
+  {checkbox: <input type="checkbox" />, commitment:<a href="#">SC-01-001</a>, contractCompany:"Vertex Properties",billingPeriod: "15/07/2021-15/08/2021", invoiceStatus:<a href="#">Under Review</a>, invoiceDates:"15/07/2021-15/08/2021",contractValue:"32,500",complete:"11.00%",currentPaymentDue:"0.00",paymentStatus:"Unpaid"},
+  {checkbox: <input type="checkbox" />,  commitment:<a href="#">SC-01-001</a>,contractCompany:"Vertex Properties",billingPeriod: "15/07/2021-15/08/2021", invoiceStatus:<a href="#">Draft</a>, invoiceDates:"15/07/2021-15/08/2021",contractValue:"15,380",complete:"0.00%",currentPaymentDue:"0.00",paymentStatus:"Unpaid"},
+  {checkbox: <input type="checkbox" />, commitment:<a href="#">SC-01-001</a>,contractCompany:"Vertex Properties",billingPeriod: "15/07/2021-15/08/2021", invoiceStatus:<a href="#">Under Review</a>, invoiceDates:"15/07/2021-15/08/2021",contractValue:"11,000",complete:"23.00%",currentPaymentDue:"0.00",paymentStatus:"Unpaid"},
+  {checkbox: "", commitment:"Grand Total: ",contractCompany:"",billingPeriod: "", invoiceStatus:"", invoiceDates:"",contractValue:"",complete:"",currentPaymentDue:"",paymentStatus:"234,567"}
 ];
 const columns = [
   {
-    dataField: 'edit',
+    dataField: 'checkbox',
     text: '',
     headerStyle: (column, colIndex) => {
         return { width: '7%', textAlign: 'center' };}
   }, {
-    dataField: 'view',
-    text: '',
+    dataField: 'commitment',
+    text: 'Commitment',
     headerStyle: (column, colIndex) => {
     return { width: '7%', textAlign: 'center' };}
   }, {
-    dataField: 'date',
-    text: 'Date',
+    dataField: 'contractCompany',
+    text: 'Contract Company',
     headerStyle: (column, colIndex) => {
     return { width: '7%', textAlign: 'center' };}
   }, {
-    dataField: 'vendor',
-    text: 'Vendor',
+    dataField: 'billingPeriod',
+    text: 'Billing Period',
     headerStyle: (column, colIndex) => {
-    return { width: '7%', textAlign: 'center' };}
+    return { width: '20%', textAlign: 'center' };}
   }, {
-    dataField: 'type',
-    text: 'Type',
+    dataField: 'invoiceStatus',
+    text: 'Invoice Status',
     headerStyle: (column, colIndex) => {
-    return { width: '50%', textAlign: 'center' };}
+    return { width: '30%', textAlign: 'center' };}
   },{
-    dataField: 'invoice',
-    text: 'Invoice #',
+    dataField: 'invoiceDates',
+    text: 'Invoice Dates',
     headerStyle: (column, colIndex) => {
     return { width: '7%', textAlign: 'center' };}
   }, {
-    dataField: 'status',
-    text: 'Status',
+    dataField: 'contractValue',
+    text: 'Contract Value',
     headerStyle: (column, colIndex) => {
         return { width: '50%', textAlign: 'center' };}
   }, {
-    dataField: 'ammount',
-    text: 'Ammount',
+    dataField: 'complete',
+    text: '% Complete',
     headerStyle: (column, colIndex) => {
         return { width: '7%', textAlign: 'center' };}
   },
   {
-    dataField: 'receivedDate',
-    text: 'Received Date',
+    dataField: 'currentPaymentDue',
+    text: 'Current Payment Due',
     headerStyle: (column, colIndex) => {
         return { width: '7%', textAlign: 'center' };}
   },
   {
-      dataField: 'paidDate',
-      text: 'Paid Date',
-      headerStyle: (column, colIndex) => {
-          return { width: '7%', textAlign: 'center' };}
-    },
-    {
-      dataField: 'blank',
-      text: '',
-      headerStyle: (column, colIndex) => {
-          return { width: '7%', textAlign: 'center' };}
-    },
-    {
-      dataField: 'delete',
-      text: '',
+      dataField: 'paymentStatus',
+      text: 'Payment Status',
       headerStyle: (column, colIndex) => {
           return { width: '7%', textAlign: 'center' };}
     }
@@ -81,17 +71,25 @@ const Invoicing = () => {
         <div>
           <div className="">
             <ul class="nav nav-tabs">
-           <a href="/invoiceSettings"></a><h3 style={{paddingLeft: 10, paddingRight: 50}}> INVOICING</h3>
+          <h3 style={{paddingLeft: 10, paddingRight: 50}}> INVOICING</h3>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/invoicing">Owner</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/subcontractor">Sub Contractor</a>
+              <a class="nav-link active" aria-current="page" href="/invoicing">Sub Contractor</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/billing-periods">Billing Periods</a>
             </li>
           </ul> <br /></div>
+          <div className="form-row mt-3">
+            <div className="col-md-12 text-right">
+            <Link className="btn btn-primary mr-2" to={"/adddirectcost/"}>
+                Export
+                </Link>
+                </div>
+            <div className="form-group col-md-4">
+                      <input className="form-control" type="text" placeholder="Search" />
+                    </div>
+                    <a href="#" className="btn btn-outline-dark mb-3">Add Filter</a>
+                    </div>
           <div className="">
           <BootstrapTable 
             hover
