8e0a7583091e9dba15c6f96310dbbcb372371789
malithya_fernando <56259098+MalithyaFernando@users.noreply.github.com>
2021-08-27 00:25:09 +0530

    Add: Backend of punch list sub-component

--- edifice-backend/node-backend/app/models/index.js
+++ edifice-backend/node-backend/app/models/index.js
@@ -42,6 +42,11 @@ db.photo = require("./photo.model.js")(sequelize, Sequelize);
 // Document component Model Classes
 db.directory = require("./directory.model")(sequelize, Sequelize);
 db.document = require("./document.model")(sequelize, Sequelize);
+// Meeting component Model Classes
+db.meetings = require("./project_management/meeting.model")(sequelize, Sequelize);
+db.meetingcategory = require("./project_management/meetingcategory.model")(sequelize, Sequelize);
+// Punch list component Model Classes
+db.punchlist = require("./project_management/punchlist.model")(sequelize, Sequelize);
 
 db.budgets = require("./budget.model.js")(sequelize, Sequelize);
 db.demo = require("./demo.model")(sequelize, Sequelize);
@@ -58,9 +63,6 @@ db.categorys = require("./equipment-category.model")(sequelize, Sequelize);
 db.crews = require("./crew.model")(sequelize, Sequelize);
 db.workers = require("./worker.model")(sequelize, Sequelize);
 
-db.meetings = require("./project_management/meeting.model")(sequelize, Sequelize);
-db.meetingcategory = require("./project_management/meetingcategory.model")(sequelize, Sequelize);
-
 //This section is for testing purposes
 db.demo1 = require("./demo1.model")(sequelize, Sequelize);
 db.demo2 = require("./demo2.model")(sequelize, Sequelize);
@@ -72,6 +74,7 @@ db.demo2.belongsTo(db.demo1,{
 })
 //Testing section ends
 
+// ----------- Project Management Starts -------------
 // One project has many departments
 db.projects.hasMany(db.departments, { as: "departments" });
 db.departments.belongsTo(db.projects, {
@@ -152,15 +155,6 @@ db.biddings.belongsTo(db.projects, {
   as: "project",
 });
 
-
-// One project has one budget-should correct this
-db.projects.hasOne(db.budgets, { as: "budgets" });
-db.budgets.belongsTo(db.projects, {
-  foreignKey: "projectId",
-  as: "project",
-});
-
-
 //
 db.roles.belongsToMany(db.users, {
   through: "user_roles",
@@ -175,60 +169,7 @@ db.users.belongsToMany(db.roles, {
   otherKey: "roleId"
 });
 
-// One project has one direct cost
-db.projects.hasOne(db.directcosts, { as: "directcosts" });
-//One project has many direct costs
-db.projects.hasMany(db.directcosts, { as: "directcosts" });
-
-db.directcosts.belongsTo(db.projects, {
-  foreignKey: "projectId",
-  as: "project",
-});
-
-// One project has many 
-db.projects.hasMany(db.budgets, { as: "budgets" });
-db.budgets.belongsTo(db.projects, {
-  foreignKey: "projectId",
-  as: "project",
-});
-
-
-/* One project has many commitments*/
-db.projects.hasMany(db.commitments, { as: "commitments" });
-db.commitments.belongsTo(db.projects, {
-  foreignKey: "projectId",
-  as: "project",
-});
-
-
-// One commitment has many sovs
-db.commitments.hasMany(db.sovs, { as: "sovs" });
-db.sovs.belongsTo(db.commitments, {
-  foreignKey: "commitmentId",
-  as: "commitment",
-});
-
-// One commitment has many sovs
-db.commitments.hasMany(db.payments, { as: "payments" });
-db.payments.belongsTo(db.commitments, {
-  foreignKey: "commitmentId",
-  as: "commitment",
-});
-
-//resource mgt
-//One category has many equipments
-db.categorys.hasMany(db.equipments, { as: "equipments" });
-db.equipments.belongsTo(db.categorys, {
-  foreignKey: "categoryId",
-  as: "categories",
-});
-
-//One crew has many workers
-db.crews.hasMany(db.workers, { as: "workers" });
-db.workers.belongsTo(db.crews, {
-  foreignKey: "crewId",
-  as: "crew",
-});
+// ---------------------------------------------------
 
 //One project has many meetings
 db.projects.hasMany(db.meetings, { as: "meetings" });
@@ -251,7 +192,51 @@ db.meetings.belongsTo(db.meetingcategory, {
   as: "mcategory",
 });
 
-// One project has many drawings
+// One project has many punch lists
+db.projects.hasMany(db.punchlist, { as: "punchlist" });
+db.punchlist.belongsTo(db.projects, {
+  foreignKey: "projectId",
+  as: "project",
+});
+// ----------- Project Management Ends -------------
+
+// ----------- Finance Management Starts -----------
+// One project has one direct cost
+db.projects.hasMany(db.directcosts, { as: "directcosts" });
+db.directcosts.belongsTo(db.projects, {
+  foreignKey: "projectId",
+  as: "project",
+});
+
+// One project has many budgets
+db.projects.hasMany(db.budgets, { as: "budgets" });
+db.budgets.belongsTo(db.projects, {
+  foreignKey: "projectId",
+  as: "project",
+});
+
+// One project has many commitments
+db.projects.hasMany(db.commitments, { as: "commitments" });
+db.commitments.belongsTo(db.projects, {
+  foreignKey: "projectId",
+  as: "project",
+});
+
+// One commitment has many sovs
+db.commitments.hasMany(db.sovs, { as: "sovs" });
+db.sovs.belongsTo(db.commitments, {
+  foreignKey: "commitmentId",
+  as: "commitment",
+});
+
+// One commitment has many sovs
+db.commitments.hasMany(db.payments, { as: "payments" });
+db.payments.belongsTo(db.commitments, {
+  foreignKey: "commitmentId",
+  as: "commitment",
+});
+
+// One project has many primecontracts
 db.projects.hasOne(db.primecontracts, { as: "primecontracts" });
 db.primecontracts.belongsTo(db.projects, {
   foreignKey: "projectId",
@@ -264,6 +249,23 @@ db.invoices.belongsTo(db.commitments, {
   foreignKey: "commitmentId",
   as: "commitment",
 });
+// ----------- Finance Management Ends -----------
+
+// ----------- Resource Management Starts --------
+//One category has many equipments
+db.categorys.hasMany(db.equipments, { as: "equipments" });
+db.equipments.belongsTo(db.categorys, {
+  foreignKey: "categoryId",
+  as: "categories",
+});
+
+//One crew has many workers
+db.crews.hasMany(db.workers, { as: "workers" });
+db.workers.belongsTo(db.crews, {
+  foreignKey: "crewId",
+  as: "crew",
+});
+// ----------- Resource Management Ends --------
 
 db.ROLES = ["user", "admin", "moderator"];
 
