23b617500302bd8838c37bd14ba24fcebad407db
vinuri-s <piyathilakev@gmail.com>
2021-09-12 22:57:58 +0530

    Add: Budget Estimates Page

--- edifice-frontend/edifice-frontend/src/App.js
+++ edifice-frontend/edifice-frontend/src/App.js
@@ -141,6 +141,7 @@ import AddEmployee from "./components/core_tools/edifice-directory/add-emp.compo
 import Report from "./components/report/report.component";
 
 import UploadExcel from "./components/financial_management/direct-costs/excelupload.component";
+import EstimateBudget from "./components/financial_management/budget/budgetestimates.component";
 
 
 class App extends Component {
@@ -402,6 +403,7 @@ class App extends Component {
             <Route path="/viewsinglesov/:id" component={ViewSingleSov} />
             <Route path="/viewbudget/:id" component={ViewSingleBudget} />
             <Route path="/excelupload/:id" component={UploadExcel} />
+            <Route path="/budgetestimates/:id" component={EstimateBudget} />
 
             {/*Report and PDF */}
             <Route path="/report/" component={Report} />
---
+++ edifice-frontend/edifice-frontend/src/components/financial_management/budget/budgetestimates.component.js
@@ -0,0 +1,246 @@
+import React, { useState, useEffect, useMemo, useRef } from "react";
+import BudgetDataService from "./../../../services/budget.service";
+import { useTable } from "react-table";
+import { Route, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+
+const BudgetEstimates = (props) => {
+  const {id}= useParams();
+  const [budgets, setBudgets] = useState([]);
+  const [searchCostCode, setSearchCostCode] = useState("");
+  const budgetsRef = useRef();
+  budgetsRef.current = budgets;
+
+
+  useEffect(() => {
+    retrieveBudgets();    
+
+  }, []);
+
+
+
+  const onChangeSearchCostCode = (e) => {
+    const searchCostCode = e.target.value;
+    setSearchCostCode(searchCostCode);
+  };
+
+  const retrieveBudgets = () => {
+    
+    BudgetDataService.getAll(id)//passing project id as id
+      .then((response) => {
+        setBudgets(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const refreshList = () => {
+    retrieveBudgets();
+    
+  };
+
+  const findByCostCode = () => {
+    BudgetDataService.findByCostCode(id,searchCostCode)
+      .then((response) => {
+        setBudgets(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const openBudget = (rowIndex) => {
+    const id = budgetsRef.current[rowIndex].id;
+    //const projectId = directcostsRef.current[rowIndex].projectId;
+
+    props.history.push("/viewbudget/"+ id);//here id is direct cost id
+  };
+
+
+
+  const deleteBudget = (rowIndex) => {
+    const id = budgetsRef.current[rowIndex].id;
+    //const projectId = directcostsRef.current[rowIndex].projectId;
+
+    BudgetDataService.remove(id)
+      .then((response) => {
+        
+        //props.history.push("/directcost/"+id);
+
+        let newBudgets = [...budgetsRef.current];
+        newBudgets.splice(rowIndex, 1);
+
+        setBudgets(newBudgets);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Id",
+        accessor: "id",
+      },
+      {
+        Header: "Cost Code",
+        accessor: "costCode",
+      },
+      {
+        Header: "Description",
+        accessor: "description",
+      },
+      {
+        Header: "Date",
+        accessor: "date",
+      },
+      {
+        Header: "Estimated Budget Amount(Rs.)",
+        accessor: "estimatedBudget",
+      },
+      {
+        Header: "",
+        accessor: "actions",
+        Cell: (props) => {
+          const rowIdx = props.row.id;
+          return (
+            <div>
+              <span onClick={() => openBudget(rowIdx)}>
+              <EditIcon></EditIcon>&nbsp;&nbsp;
+              </span>
+
+              <span onClick={() => deleteBudget(rowIdx)}>
+                <DeleteIcon></DeleteIcon>
+              </span>
+            </div>
+          );
+        },
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data: budgets
+  });
+
+  return (
+    <div>
+        <h3> BUDGET ESTIMATES</h3>
+               <h6>Estimated the Project Budget.</h6> 
+                <hr /><br />
+
+               <div className="form-row mt-3">
+            <div className="col-md-12 text-right">
+            <Link className="btn btn-primary mr-2" to={"/addbudget/"+id}>{/*check this again*/}
+                + Create
+                </Link>
+                <Link className="btn btn-primary mr-2" to={"/adddirectcost/"+1}>
+                Import 
+                </Link>
+                <Link className="btn btn-primary mr-2" to={"/adddirectcost/"+1}>
+                Export 
+                </Link>
+                </div>
+      <div className="form-group col-md-4">
+        <div className="input-group mb-3">
+         {/* <input
+            type="text"
+            className="form-control"
+            placeholder="Search by cost code"
+            value={searchCostCode}
+            onChange={onChangeSearchCostCode}
+          />*/}
+           <select 
+                
+                id="costCode"
+           
+                
+                name="costCode"
+                className="form-control"
+            placeholder="Search by cost code"
+            value={searchCostCode}
+            onChange={onChangeSearchCostCode}
+              >
+                <option  selected value="">All</option>
+             {budgets &&
+                budgets.map((budget, index) => (
+                <option
+                    value={budget.costCode}
+                    onChange={onChangeSearchCostCode}
+                    key={index}
+                >
+                {/* unit data */}
+                {budget.costCode}
+                </option>
+                ))}
+
+                {/*<option></option>
+                <option>001-Maintenance Equipment</option>
+                <option>002-Sodding</option>
+                <option>003-Visual Display Boards</option>
+                <option>004-Site Clearing</option>
+                <option>005-Dewatering</option>*/}
+             
+              </select>
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={findByCostCode}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <table
+          className="table table-striped table-bordered"
+          {...getTableProps()}
+        >
+          <thead className="Table-header">
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+  </div>
+  </div>
+  );
+};
+
+export default BudgetEstimates; 
