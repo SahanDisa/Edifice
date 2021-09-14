9328a7d054df50a9a05d8a776b7ebf3372262a5d
vinuri-s <piyathilakev@gmail.com>
2021-09-14 22:04:04 +0530

    Add: Import estimated budget backend

---
+++ edifice-frontend/edifice-frontend/src/components/financial_management/budget/bexcelupload.component.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+import BExcelService from "../../../services/bexcelupload.service";
+import BudgetDataService from "../../../services/budget.service";
+import { Link } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
+
+// excel file upload
+const BExcelUploadFiles = (props) => {
+  const {id}= useParams();
+  
+    const [selectedFiles, setSelectedFiles] = useState(undefined);
+    const [currentFile, setCurrentFile] = useState(undefined);
+    const [progress, setProgress] = useState(0);
+    const [message, setMessage] = useState("");
+    const [budgets, setBudgets] = useState([]);
+   
+   
+  
+    const [fileInfos, setFileInfos] = useState([]);
+    const selectFile = (event) => {
+        setSelectedFiles(event.target.files);
+    };
+
+    const retrieveBudgets = () => {
+    
+      BudgetDataService.getAll(id)//passing project id as id
+        .then((response) => {
+          setBudgets(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    };
+
+    const upload = () => {
+        let currentFile = selectedFiles[0];
+    
+        setProgress(0);
+        setCurrentFile(currentFile);
+    
+        BExcelService.upload(currentFile, (event) => {
+          setProgress(Math.round((100 * event.loaded) / event.total));
+        })
+          .then((response) => {
+            setMessage(response.data.message);
+            return BExcelService.getFiles();
+          })
+          .then((files) => {
+            setFileInfos(files.data);
+          })
+          .catch(() => {
+            setProgress(0);
+            setMessage("Could not upload the file!");
+            setCurrentFile(undefined);
+          });
+    
+        setSelectedFiles(undefined);
+    };
+    useEffect(() => {
+        BExcelService.getFiles().then((response) => {
+          setFileInfos(response.data);
+        });
+        retrieveBudgets();  
+
+    }, []);
+    return (
+        <div>
+     
+          {/*<div className="modal-dialog modal-dialog-centered" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalCenterTitle">Import Direct Costs</h5>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => {retrieveDirectCosts();setTimeout(this.setState.bind(this, {position:1}), 3000); }} >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+    <div className="modal-body">*/}
+      <div>
+        <h4>Import Estimated Budget</h4>  
+        <hr />
+          {currentFile && (
+            <div className="progress">
+              <div
+                className="progress-bar progress-bar-info progress-bar-striped"
+                role="progressbar"
+                aria-valuenow={progress}
+                aria-valuemin="0"
+                aria-valuemax="100"
+                style={{ width: progress + "%" }}
+              >
+                {progress}%
+              </div>
+            </div>
+          )}
+    
+          <label className="btn btn-default">
+            <input type="file" onChange={selectFile} />
+          </label>
+    
+          <button
+            className="btn btn-success"
+            disabled={!selectedFiles}
+            onClick={upload}
+          >
+            Upload
+          </button><br />
+          <Link  to={"/budgetestimates/"+id} className="btn btn-success">View Estimated Budget</Link>
+    
+          <div className="alert alert-light" role="alert">
+            {message}
+          </div>
+
+         </div>
+              { /* <div className="modal-footer">
+   
+          <Link  to={"/directcost/"+id} className="btn btn-success">View Direct Costs</Link>
+         
+                </div>
+              </div>
+            </div>
+    
+          </div>*/}
+        </div>
+    );
+  };
+
+export default BExcelUploadFiles;
