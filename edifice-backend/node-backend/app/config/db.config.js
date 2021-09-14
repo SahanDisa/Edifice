bc5d9b2810e3ed989596217c3ee0682bb8815031
randiepathirage <randiepathirage@gmail.com>
2021-09-15 00:23:24 +0530

    Fixed error in crews

--- edifice-backend/node-backend/app/config/db.config.js
+++ edifice-backend/node-backend/app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: "localhost",
     USER: "root",
     //PASSWORD: "malithya",
-    PASSWORD: "",
+    PASSWORD: "randie98",
     DB: "edificedb_node",
     dialect: "mysql",
     pool: {
