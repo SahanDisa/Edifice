4254b72c5638d255380d5755c7209ce1a65b4cb7
vinuri-s <piyathilakev@gmail.com>
2021-09-19 16:34:19 +0530

    Add: Cost Code Backend

---
+++ edifice-backend/node-backend/app/models/costcode.model.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, Sequelize) => {
+    const CostCode = sequelize.define("costcode", {
+      costCode: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.DATEONLY
+      },
+      published: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },  
+
+    
+    }, {
+        freezeTableName: true,
+    });
+  
+    return CostCode;
+};
