c52ca668da561c90bfbe8f85a6693b1fd03acd45
malithya_fernando <56259098+MalithyaFernando@users.noreply.github.com>
2021-09-20 11:31:14 +0530

    Update: Action plan component

--- edifice-frontend/edifice-frontend/src/components/project_management/actionplan/addapsection.component.js
+++ edifice-frontend/edifice-frontend/src/components/project_management/actionplan/addapsection.component.js
@@ -2,12 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import ActionPlanSectionDataService from "./../../../services/project_management/actionplansection.service";
 import ActionPlanTypeDataService from "../../../services/project_management/actionplantype.service";
-import Timeline from '@material-ui/lab/Timeline';
-import TimelineItem from '@material-ui/lab/TimelineItem';
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
-import TimelineConnector from '@material-ui/lab/TimelineConnector';
-import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineDot from '@material-ui/lab/TimelineDot';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 
 export default class AddAPSection extends Component {
   constructor(props) {
@@ -20,17 +15,18 @@ export default class AddAPSection extends Component {
         this.saveAPsection = this.saveAPsection.bind(this);
 
         this.state = {
-        id: null,
-        title: "",
-        reftype: "",
-        refid: "",
-        acceptance: "",
-        duedate: "",
-        actionplanId: this.props.actionplanId,
-        
-        actionplantypes: [],
-        currentIndex: -1,
-        submitted: false,
+            id: null,
+            title: "",
+            reftype: "",
+            refid: "",
+            acceptance: "",
+            duedate: "",
+            actionplanId:"",
+            projectId: "",
+            
+            actionplantypes: [],
+            currentIndex: -1,
+            submitted: false,
         };
     }
     
@@ -81,7 +77,7 @@ export default class AddAPSection extends Component {
             refid: response.data.refid,
             acceptance: response.data.acceptance,
             duedate: response.data.duedate,
-            actionplanId: response.data.actionplanId,
+            actionplanId: this.props.match.params.apid,
 
             submitted: true
         });
@@ -90,106 +86,91 @@ export default class AddAPSection extends Component {
     }
 
     render() {
-        const {actionplanId, currentIndex, actionplantypes} = this.state;
+        const {actionplanId, currentIndex, actionplantypes, projectId} = this.state;
         return (
             <div className="container">
-                {/* {this.state.submitted ? (
-                <div>
-                <center>
-                    <h4>Action Plan details successfully submitted!</h4>
-                    <Link to={"/actionplan/"+actionplanId} className="btn btn-primary mr-2"  style={{ 'text-decoration': 'none' }}>
-                    Back Home
-                    </Link>
-                    <Link to={"/addactionplan/"+actionplanId} className="btn btn-primary mr-2"  style={{ 'text-decoration': 'none' }}>
-                    Add Action Plan
-                    </Link>
-                </center>
-                </div>
-                ) : ( */}
                 <div class="container">
-                    <div className="modal-dialog modal-lg modal-dialog-centered" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalCenterTitle">Add New Action Plan Section</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        </div>
-                        <div className="modal-body">
-                        <div className="">
-                            <div className="form-row">
-                                <div className="form-group col-md-4">
-                                    <label htmlFor="title">Title</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="title"
-                                        required
-                                        value={this.state.title}
-                                        onChange={this.onChangeTitle}
-                                        name="title"
-                                    />
-                                </div>
-                                <div className="form-group col-md-5">
-                                    <label htmlFor="acceptance">Acceptance</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="acceptance"
-                                        required
-                                        value={this.state.acceptance}
-                                        onChange={this.onChangeAcceptance}
-                                        name="acceptance"
-                                    />
-                                </div>
-                                <div className="form-group col-md-3">
-                                    <label htmlFor="duedate">Due Date</label>
-                                    <input
-                                        className="form-control"
-                                        name="duedate"
-                                        value={this.state.duedate}
-                                        onChange={this.onChangeDuedate}
-                                        type="date"
-                                        min=""
-                                        required
-                                    />
-                                </div>
-                            </div>
-                            <div className="form-row">
-                                <div className="form-group col-md-4">
-                                <label htmlFor="reftype">Ref type</label>
+                    <h2>Add New Action Plan Section</h2>
+                    <Breadcrumbs aria-label="breadcrumb">
+                        <Link color="inherit" to="/home">Home</Link>
+                        <Link color="inherit" to={"/projectmanagementhome/"+projectId}>App Dashboard</Link>
+                        <Link color="inherit" to={"/actionplan/" + projectId}>Action Plan</Link>
+                        <Link color="inherit" to={"/actionplansinglepage/" + projectId + "/" + actionplanId}>Action Plan Single Page</Link>
+                        <Link color="inherit" aria-current="page" className="disabledLink">Add Action Plan Section</Link>
+                    </Breadcrumbs><hr/>
+                    <div className="">
+                        <div className="form-row">
+                            <div className="form-group col-md-4">
+                                <label htmlFor="title">Title</label>
                                 <input
                                     type="text"
                                     className="form-control"
-                                    id="reftype"
+                                    id="title"
                                     required
-                                    value={this.state.reftype}
-                                    onChange={this.onChangeReftype}
-                                    name="reftype"
+                                    placeholder="Enter a title to the section "
+                                    value={this.state.title}
+                                    onChange={this.onChangeTitle}
+                                    name="title"
                                 />
-                                </div>
-                                <div className="form-group col-md-8">
-                                    <label htmlFor="refid">Ref Id</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="refid"
-                                        required
-                                        value={this.state.refid}
-                                        onChange={this.onChangeRefid}
-                                        name="refid"
-                                    />
-                                </div>
+                            </div>
+                            <div className="form-group col-md-5">
+                                <label htmlFor="acceptance">Acceptance</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="acceptance"
+                                    required
+                                    placeholder="Enter acceptance"
+                                    value={this.state.acceptance}
+                                    onChange={this.onChangeAcceptance}
+                                    name="acceptance"
+                                />
+                            </div>
+                            <div className="form-group col-md-3">
+                                <label htmlFor="duedate">Due Date</label>
+                                <input
+                                    className="form-control"
+                                    name="duedate"
+                                    placeholder="Enter due date"
+                                    value={this.state.duedate}
+                                    onChange={this.onChangeDuedate}
+                                    type="date"
+                                    min=""
+                                    required
+                                />
                             </div>
                         </div>
-                        </div>
-                        <div className="modal-footer">
-                        <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.saveAPitem}>Create</button>
+                        <div className="form-row">
+                            <div className="form-group col-md-4">
+                            <label htmlFor="reftype">Reference type</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="reftype"
+                                placeholder="Enter what type of files you will be refered to"
+                                required
+                                value={this.state.reftype}
+                                onChange={this.onChangeReftype}
+                                name="reftype"
+                            />
+                            </div>
+                            <div className="form-group col-md-8">
+                                <label htmlFor="refid">Ref Id</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="refid"
+                                    required
+                                    value={this.state.refid}
+                                    onChange={this.onChangeRefid}
+                                    name="refid"
+                                />
+                            </div>
                         </div>
                     </div>
-                    </div>
+                    <button onClick={this.saveActionPlan} className="btn btn-success mr-2">Create Action Plan</button>
+                    <a href="/actionplan">Cancel</a>
                 </div>
-                {/* )} */}
             </div>
         );
     }
