ffe5c5d585c0c0e170150be05d914e6c3041a659
randiepathirage <randiepathirage@gmail.com>
2021-09-20 12:28:04 +0530

    Updated timesheet add workes

--- edifice-frontend/edifice-frontend/src/components/resource_management/Crew/view-worker.component.js
+++ edifice-frontend/edifice-frontend/src/components/resource_management/Crew/view-worker.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Modal} from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import DeleteIcon from '@material-ui/icons/Delete';
 import WorkersDataService from "./../../../services/worker.service";
 
@@ -13,18 +13,18 @@ class ViewWorker extends Component {
       //currentIndex: -1,
       //content: "",
       wId: this.props.id,
-      fristName:this.props.fName,
-      lastName:this.props.lName,
-      mobile:this.props.mobile
+      fristName: this.props.fName,
+      lastName: this.props.lName,
+      mobile: this.props.mobile
     };
   }
 
-  deleteWorker() {    
+  deleteWorker() {
     WorkersDataService.delete(this.state.wId)
       .then(response => {
         console.log(response.data);
         window.location.reload();
-       // this.props.history.push('/drawings/'+this.state.pid);
+        // this.props.history.push('/drawings/'+this.state.pid);
       })
       .catch(e => {
         console.log(e);
@@ -34,44 +34,44 @@ class ViewWorker extends Component {
 
 
 
-    render() {
-      const { wId,fristName,lastName, mobile} = this.state;
-      //console.log(wId)
-        return (  
-        <div>
-          <Modal.Header closeButton> 
-            <h5 className="modal-title" id="exampleModalCenterTitle">Edit Worker Details</h5>
-          </Modal.Header>
-          <Modal.Body>
-            <div>
-              <label htmlFor=""><b>Id:</b> {wId}</label>
-              <label htmlFor=""><b>First Name:</b> {fristName}</label>
-              <label htmlFor=""><b>Last Name:</b> {lastName}</label>
-              <label htmlFor=""><b>Mobile:</b> {mobile}</label>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
+  render() {
+    const { wId, fristName, lastName, mobile } = this.state;
+    //console.log(wId)
+    return (
+      <div>
+        <Modal.Header closeButton>
+          <h5 className="modal-title" id="exampleModalCenterTitle">Edit Worker Details</h5>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <label htmlFor=""><b>Id:</b> {wId}</label>
+            <label htmlFor=""><b>First Name:</b> {fristName}</label>
+            <label htmlFor=""><b>Last Name:</b> {lastName}</label>
+            <label htmlFor=""><b>Mobile:</b> {mobile}</label>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
 
-            <button 
-                type="button" 
-                className="btn btn-danger" 
-                data-toggle="modal" 
-                data-target="#viewWorker"
-                onClick={this.deleteWorker}>
-                  Delete 
-                  <DeleteIcon/>
-              </button>
+          <button
+            type="button"
+            className="btn btn-danger"
+            data-toggle="modal"
+            data-target="#viewWorker"
+            onClick={this.deleteWorker}>
+            Delete
+            <DeleteIcon />
+          </button>
 
-              <button 
+          {/* <button 
                 type="button" 
                 className="btn btn-success">
                   Check Worked Hours
-              </button>
+              </button> */}
 
-          </Modal.Footer>
-        </div>
-        );
-    }
+        </Modal.Footer>
+      </div>
+    );
   }
+}
 
 export default ViewWorker;
